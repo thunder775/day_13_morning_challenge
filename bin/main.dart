09b6feaaacac9a5675c11403a3799c18a9af9637
McLarenCollege <hello@mclarencollege.in>
2019-10-17 08:15:00 +0530

    Morning challenge modifications

--- bin/main.dart
+++ bin/main.dart
@@ -1,24 +1,27 @@
 // Challenge 1
+// Read the two problems below and write test cases for them, make sure test cases run and fail
+
+// Challenge 2
 // Repeat the Shorter String
 // Write a function that repeats the shorter string until it is equal to the length of the longer string.
 // Examples:
 // lengthen("abcdefg", "ab") ➞ "abababa"
 // lengthen("ingenius", "forest") ➞ "forestfo"
+String lengthen(String word1, String word2){
+  return null;
+}
 
-
-// Challenge 2
+// Challenge 3
 // Given what is supposed to be typed and what is actually typed,
 // write a function that returns the broken key(s). The function looks like:
 // findBrokenKeys(correct phrase, what you actually typed)
 // Examples
 // findBrokenKeys("happy birthday", "hawwy birthday") ➞ ["p"]
 // findBrokenKeys("beethoven", "affthoif5") ➞ ["b", "e", "v", "n"]
+List findBrokenKeys(String correct, String typed){
+  return null;
+}
 
-// Challenge 3
-// Create a function that takes a number and returns its multiplicative persistence,
-// which is the number of times you must multiply the digits in num until you reach a single digit.
-// bugger(39) ➞ 3
-/// Because 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4 and 4 has only one digit.
 
 main() {
 }
