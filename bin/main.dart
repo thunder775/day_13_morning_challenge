a4bcfcbaaeeff123d48a2fabcb1dd5ca803a2bba
McLarenCollege <hello@mclarencollege.in>
2019-10-17 08:00:05 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,24 @@
+// Challenge 1
+// Repeat the Shorter String
+// Write a function that repeats the shorter string until it is equal to the length of the longer string.
+// Examples:
+// lengthen("abcdefg", "ab") ➞ "abababa"
+// lengthen("ingenius", "forest") ➞ "forestfo"
+
+
+// Challenge 2
+// Given what is supposed to be typed and what is actually typed,
+// write a function that returns the broken key(s). The function looks like:
+// findBrokenKeys(correct phrase, what you actually typed)
+// Examples
+// findBrokenKeys("happy birthday", "hawwy birthday") ➞ ["p"]
+// findBrokenKeys("beethoven", "affthoif5") ➞ ["b", "e", "v", "n"]
+
+// Challenge 3
+// Create a function that takes a number and returns its multiplicative persistence,
+// which is the number of times you must multiply the digits in num until you reach a single digit.
+// bugger(39) ➞ 3
+/// Because 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4 and 4 has only one digit.
+
+main() {
+}
